611aacd8e20cd4613b56314e8d5a2cd9e31bb5cb
Naphat Amundsen <naphat@live.no>
2023-09-03 14:07:48 +0200

    styling stuff

--- unix/stow/vim/.config/lvim/config.lua
+++ unix/stow/vim/.config/lvim/config.lua
@@ -1,45 +1,48 @@
 ---- Custom plugins ----
 lvim.plugins = {
-    { "sainnhe/sonokai" },
-    { "tyru/open-browser.vim" },
-    { "aklt/plantuml-syntax" },
-    { "ThePrimeagen/harpoon" },
-    { "simrat39/rust-tools.nvim" },
-    { "tpope/vim-surround" },
-    { "mfussenegger/nvim-dap-python" },
+	{ "sainnhe/sonokai" },
+	{ "tyru/open-browser.vim" },
+	{ "aklt/plantuml-syntax" },
+	{ "ThePrimeagen/harpoon" },
+	{ "simrat39/rust-tools.nvim" },
+	{ "tpope/vim-surround" },
+	{ "mfussenegger/nvim-dap-python" },
 
-    {
-        "weirongxu/plantuml-previewer.vim",
-        dependencies = "tyru/open-browser.vim"
-    },
-    {
-        "iamcco/markdown-preview.nvim",
-        build = "cd app && npm install",
-        init = function()
-            vim.g.mkdp_filetypes = { "markdown" }
-        end,
-        ft = { "markdown" }
-    },
-    {
-        "zbirenbaum/copilot-cmp",
-        event = "InsertEnter",
-        dependencies = { "zbirenbaum/copilot.lua" },
-        config = function()
-            vim.defer_fn(function()
-                require("copilot").setup()
-                require("copilot_cmp").setup()
-            end, 100)
-        end,
-    },
-    {
-        "akinsho/flutter-tools.nvim",
-        dependencies = "nvim-lua/plenary.nvim"
-    },
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = { "tpope/vim-dadbod", "kristijanhusak/vim-dadbod-completion" },
+	},
+
+	{
+		"weirongxu/plantuml-previewer.vim",
+		dependencies = "tyru/open-browser.vim",
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		event = "InsertEnter",
+		dependencies = { "zbirenbaum/copilot.lua" },
+		config = function()
+			vim.defer_fn(function()
+				require("copilot").setup()
+				require("copilot_cmp").setup()
+			end, 100)
+		end,
+	},
+	{
+		"akinsho/flutter-tools.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+	},
 }
 
 ---- General ----
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
 vim.opt.relativenumber = true
 
 lvim.log.level = "warn"
@@ -56,21 +59,16 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 lvim.builtin.treesitter.auto_install = true
 
-
 ---- Aesthetics ----
 lvim.transparent_window = true
 lvim.colorscheme = "sonokai"
 
-lvim.autocommands = {
-    { "ColorScheme", { command = "hi NvimTreeEndOfBuffer ctermbg=none guibg=none" } },
-    { "BufWinEnter", { command = ":set formatoptions-=cro" } }
-}
-
+vim.api.nvim_create_autocmd("ColorScheme", { command = "hi NvimTreeEndOfBuffer ctermbg=none guibg=none" })
+vim.api.nvim_create_autocmd("BufWinEnter", { command = ":set formatoptions-=cro" })
 
 ---- Custom options and such ----
 vim.opt.relativenumber = true
 vim.opt.smartindent = true
-
 
 ---- Keybindings ----
 lvim.keys.normal_mode["<leader>o"] = "o<ESC>"
@@ -82,12 +80,21 @@ lvim.builtin.terminal.open_mapping = "<c-t>"
 lvim.builtin.which_key.mappings["lR"] = { "<cmd>LspRestart<cr>", "Lsp Restart" }
 lvim.builtin.which_key.mappings["Q"] = { "<cmd>qa!<cr>", "Quit all" }
 
+-- Dadbod
+vim.api.nvim_create_autocmd(
+	"FileType",
+	{ pattern = { "dbui" }, command = ":nmap <buffer> l <Plug>(DBUI_SelectLineVsplit)" }
+)
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "sql", "mysql", "plsql" },
+	command = "lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })",
+})
 
 -- Harpoon
 lvim.keys.normal_mode["ø"] = "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>"
-lvim.keys.normal_mode["Ø"] = ":lua require(\"harpoon.mark\").add_file()<CR>"
-require("telescope").load_extension('harpoon')
-
+lvim.keys.normal_mode["Ø"] = ':lua require("harpoon.mark").add_file()<CR>'
+require("telescope").load_extension("harpoon")
 
 -- Imports
 require("user.lsp")
