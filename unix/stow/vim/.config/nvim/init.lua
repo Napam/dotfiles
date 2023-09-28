1364c80f9e4e8a9384dceaad4e36b2d60d529987
Naphat Amundsen <naphat@live.no>
2023-09-28 21:16:09 +0200

    fuzzy telescope

--- unix/stow/vim/.config/nvim/init.lua
+++ unix/stow/vim/.config/nvim/init.lua
@@ -1,83 +1,86 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  "folke/which-key.nvim",
-  "sainnhe/sonokai",
-  "akinsho/bufferline.nvim",
-  "lewis6991/gitsigns.nvim",
-  "lukas-reineke/indent-blankline.nvim",
-  "nvim-treesitter/nvim-treesitter",
-  "mbbill/undotree",
-  "tpope/vim-surround",
-  "RRethy/vim-illuminate",
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
-  {
-    "kdheepak/lazygit.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
-  {
-    "numToStr/Comment.nvim",
-    lazy = false,
-  },
+	"folke/which-key.nvim",
+	"sainnhe/sonokai",
+	"akinsho/bufferline.nvim",
+	"lewis6991/gitsigns.nvim",
+	"lukas-reineke/indent-blankline.nvim",
+	"nvim-treesitter/nvim-treesitter",
+	"mbbill/undotree",
+	"tpope/vim-surround",
+	"RRethy/vim-illuminate",
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+		},
+	},
+	{
+		"numToStr/Comment.nvim",
+		lazy = false,
+	},
 
-  -- Lsp and autcompletion
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v2.x",
-    dependencies = {
-      -- LSP Support
-      { "neovim/nvim-lspconfig" },
-      { "williamboman/mason.nvim" },
-      { "williamboman/mason-lspconfig.nvim" },
+	-- Lsp and autcompletion
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		dependencies = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-      -- Autocompletion
-      { "hrsh7th/nvim-cmp" },
-      { "hrsh7th/cmp-nvim-lsp" },
-      { "hrsh7th/cmp-nvim-lua" },
-      { "hrsh7th/cmp-buffer" },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
+			{ "hrsh7th/cmp-buffer" },
 
-      -- Snippets
-      { "L3MON4D3/LuaSnip" },
-      { "saadparwaiz1/cmp_luasnip" },
-      { "rafamadriz/friendly-snippets" },
-    },
-  },
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	},
 })
 
 require("user.options")
@@ -89,9 +92,9 @@ require("user.lsp")
 
 -- Bufferline
 require("bufferline").setup({
-  options = {
-    offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
-  },
+	options = {
+		offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+	},
 })
 
 -- Comment
@@ -105,10 +108,10 @@ require("ibl").setup({})
 
 -- Treesitter
 require("nvim-treesitter.configs").setup({
-  auto_install = true,
-  highlight = {
-    enable = true,
-  },
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
 })
 
 -- Illuminate
@@ -116,3 +119,6 @@ require("illuminate").configure()
 
 -- Lualine
 require("lualine").setup()
+
+-- Telescope
+require("telescope").load_extension("fzf")
