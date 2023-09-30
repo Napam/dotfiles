f1b6a3b7899323dca543b6106fbc5a608cd74bab
Naphat Amundsen <naphat@live.no>
2023-09-30 22:28:48 +0200

    bufferkill

--- unix/stow/vim/.config/nvim/lua/user/commands.lua
+++ unix/stow/vim/.config/nvim/lua/user/commands.lua
@@ -1,45 +1,16 @@
-local function buffer_kill(force)
-  local kill_command = "bd"
+local api = vim.api
+local bo = vim.bo
 
-  local bo = vim.bo
-  local api = vim.api
-  local fmt = string.format
-  local fn = vim.fn
+local function buffer_kill()
+  local kill_command = "bd!"
+
 
   local bufnr = api.nvim_get_current_buf()
-  local bufname = api.nvim_buf_get_name(bufnr)
-
-  if not force then
-    local choice
-    if bo[bufnr].modified then
-      choice = fn.confirm(fmt([[Save changes to "%s"?]], bufname), "&Yes\n&No\n&Cancel")
-      if choice == 1 then
-        vim.api.nvim_buf_call(bufnr, function()
-          vim.cmd("w")
-        end)
-      elseif choice == 2 then
-        force = true
-      else
-        return
-      end
-    elseif api.nvim_buf_get_option(bufnr, "buftype") == "terminal" then
-      choice = fn.confirm(fmt([[Close "%s"?]], bufname), "&Yes\n&No\n&Cancel")
-      if choice == 1 then
-        force = true
-      else
-        return
-      end
-    end
-  end
 
   -- Get list of windows IDs with the buffer to close
   local windows = vim.tbl_filter(function(win)
     return api.nvim_win_get_buf(win) == bufnr
   end, api.nvim_list_wins())
-
-  if force then
-    kill_command = kill_command .. "!"
-  end
 
   -- Get list of active buffers
   local buffers = vim.tbl_filter(function(buf)
@@ -68,6 +39,4 @@ local function buffer_kill(force)
   end
 end
 
-
-
-vim.api.nvim_create_user_command("BufferKill", buffer_kill, { force = true })
+vim.api.nvim_create_user_command("BufferKill", buffer_kill, {})
