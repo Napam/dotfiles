66fce4c17de4c3a69e4b8742dc690779e6318861
Naphat Amundsen <naphat@live.no>
2023-12-11 10:47:55 +0100

    mdpreview config

--- unix/stow/vim/.config/nvim/init.lua
+++ unix/stow/vim/.config/nvim/init.lua
@@ -236,3 +236,20 @@ require('nvim-autopairs').setup({})
 
 -- Oil
 require('oil').setup({})
+
+
+-- Markdown preview
+vim.g.mkdp_preview_options = {
+  mkit = {},
+  katex = {},
+  uml = {},
+  maid = {},
+  disable_sync_scroll = 0,
+  sync_scroll_type = 'middle',
+  hide_yaml_meta = 1,
+  sequence_diagrams = {},
+  flowchart_diagrams = {},
+  content_editable = false,
+  disable_filename = 1,
+  toc = {}
+}
