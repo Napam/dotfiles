debe265f12abc93599f0bcabe6c714451816c734
Naphat Amundsen <naphat@live.no>
2024-04-20 14:38:26 +0200

    update fzf dotfiles

--- unix/stow/fzf/dot-fzf.zsh
+++ unix/stow/fzf/dot-fzf.zsh
@@ -4,10 +4,4 @@ if [[ ! "$PATH" == */Users/naphat/.fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/Users/naphat/.fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/Users/naphat/.fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/Users/naphat/.fzf/shell/key-bindings.zsh"
+eval "$(fzf --zsh)"
