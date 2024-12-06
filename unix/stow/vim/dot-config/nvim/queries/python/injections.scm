56f9da84d1bb3bd5b58db367fd6f74446cbe4c13
Naphat Amundsen <naphat@live.no>
2024-12-06 07:32:36 +0100

    more sql injection

--- unix/stow/vim/dot-config/nvim/queries/python/injections.scm
+++ unix/stow/vim/dot-config/nvim/queries/python/injections.scm
@@ -1,4 +1,4 @@
 (call
-  function: (attribute attribute: (identifier) @id (#match? @id "executemany|execute|read_sql"))
+  function: (attribute attribute: (identifier) @id (#match? @id "executemany|execute|read_sql|text"))
   arguments: (argument_list
     (string (string_content) @injection.content (#set! injection.language "sql"))))
