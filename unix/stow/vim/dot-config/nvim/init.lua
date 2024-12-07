5012ea1d988d9e324b403cfc140dedd75adee681
Naphat Amundsen <naphat@live.no>
2024-12-07 10:10:43 +0100

    update nvim config treesitter dotfiles

--- unix/stow/vim/dot-config/nvim/init.lua
+++ unix/stow/vim/dot-config/nvim/init.lua
@@ -223,7 +223,7 @@ require("nvim-treesitter.configs").setup({
     enable = true,
   },
   injections = {
-    enable = true
+    enable = true,
   },
   indent = {
     enable = true,
@@ -280,12 +280,31 @@ require("lualine").setup()
 
 -- Telescope
 require("telescope").load_extension("fzf")
+
+-- Clone the default Telescope configuration
+local vimgrep_arguments = {
+  table.unpack(require("telescope.config").values.vimgrep_arguments),
+}
+
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+-- I don't want to search in the `.git` directory.
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
 require("telescope").setup({
   defaults = {
+    vimgrep_arguments = vimgrep_arguments,
     mappings = {
       n = {
         ["q"] = require("telescope.actions").close,
       },
+    },
+  },
+  pickers = {
+    find_files = {
+      -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+      find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
     },
   },
 })
