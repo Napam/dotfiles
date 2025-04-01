60daff74f086528585dcdc68925ce2d9c38ae7ed
Naphat Amundsen <naphat@live.no>
2025-04-01 13:06:36 +0200

    emulate tpopes vim surround keybinds in mini surround

--- unix/stow/vim/dot-config/nvim/lua/plugins/mini.lua
+++ unix/stow/vim/dot-config/nvim/lua/plugins/mini.lua
@@ -11,13 +11,9 @@ return {
     version = "*",
     opts = {
       mappings = {
-        add = "gsa", -- Add surrounding in Normal and Visual modes
-        delete = "gsd", -- Delete surrounding
-        find = "gsf", -- Find surrounding (to the right)
-        find_left = "gsF", -- Find surrounding (to the left)
-        highlight = "gsh", -- Highlight surrounding
-        replace = "gsr", -- Replace surrounding
-        update_n_lines = "gsn", -- Update `n_lines`
+        add = "S",              -- Add surrounding in Normal and Visual modes
+        delete = "ds",          -- Delete surrounding
+        replace = "cs",         -- Replace surrounding
       },
     },
   },
