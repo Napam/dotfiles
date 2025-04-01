7386ab857f20b1a9e87036f4af0d462dbe2a3e97
Naphat Amundsen <naphat@live.no>
2025-04-01 08:21:07 +0200

    update python injections

--- unix/stow/vim/dot-config/nvim/queries/python/injections.scm
+++ unix/stow/vim/dot-config/nvim/queries/python/injections.scm
@@ -6,7 +6,11 @@
   arguments: (argument_list
     (string (string_content) @injection.content (#set! injection.language "sql"))))
 
+
 (call
-  function: (attribute attribute: (identifier) @id (#match? @id "markdown"))
+  function: [
+    (attribute attribute: (identifier) @id)
+    (identifier) @id
+  ] (#match? @id "markdown")
   arguments: (argument_list
     (string (string_content) @injection.content (#set! injection.language "markdown"))))
