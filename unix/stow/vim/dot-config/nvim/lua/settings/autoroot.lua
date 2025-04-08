35f451ee5027d453e7c000662e62051888af66da
Naphat Amundsen <naphat@live.no>
2025-04-08 08:29:47 +0200

    update autoroot autocmd to use vim.fs.root

--- unix/stow/vim/dot-config/nvim/lua/settings/autoroot.lua
+++ unix/stow/vim/dot-config/nvim/lua/settings/autoroot.lua
@@ -6,36 +6,28 @@ local find_root = function(buf_id, names)
   buf_id = buf_id or 0
   names = names or { '.git', 'Makefile' }
 
-  -- Compute directory to start search from. NOTEs on why not using file path:
-  -- - This has better performance because `vim.fs.find()` is called less.
-  -- - *Needs* to be a directory for callable `names` to work.
-  -- - Later search is done including initial `path` if directory, so this
-  --   should work for detecting buffer directory as root.
-  local path = vim.api.nvim_buf_get_name(buf_id)
-  if path == '' then
-    path = vim.loop.cwd() or ''
-  end
-  local dir_path = vim.fs.dirname(path)
+  local source_path = vim.api.nvim_buf_get_name(buf_id)
+  local cwd = vim.loop.cwd()
 
-  -- Try using cache
-  local res = root_cache[dir_path]
-  if res ~= nil then return res end
-
-  -- Find root
-  local root_file = vim.fs.find(names, { path = dir_path, upward = true })[1]
-  if root_file ~= nil then
-    res = vim.fs.dirname(root_file)
+  if #source_path == 0 and cwd ~= nil then
+    source_path = cwd
+  else
+    return
   end
+
+  local result = root_cache[source_path]
+  if result ~= nil then return result end
+
+  result = vim.fs.root(vim.loop.cwd(), names)
 
   -- Use absolute path to an existing directory
-  if type(res) ~= 'string' then return end
-  res = vim.fs.normalize(vim.fn.fnamemodify(res, ':p'))
-  if vim.fn.isdirectory(res) == 0 then return end
+  if type(result) ~= 'string' then return end
+  result = vim.fs.normalize(vim.fn.fnamemodify(result, ':p'))
+  if vim.fn.isdirectory(result) == 0 then return end
 
-  -- Cache result per directory path
-  root_cache[dir_path] = res
+  root_cache[source_path] = result
 
-  return res
+  return result
 end
 
 
