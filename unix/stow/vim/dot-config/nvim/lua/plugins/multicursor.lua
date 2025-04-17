6cfbace7f925707ff95fead21e7e091412c44e99
Naphat Amundsen <naphat@live.no>
2025-04-17 22:29:09 +0200

    multicursor, update lazy

---
+++ unix/stow/vim/dot-config/nvim/lua/plugins/multicursor.lua
@@ -0,0 +1,94 @@
+return {
+  "jake-stewart/multicursor.nvim",
+  branch = "main",
+  config = function()
+    local mc = require("multicursor-nvim")
+    mc.setup()
+
+    local set = vim.keymap.set
+
+    -- Add cursors above/below the main cursor.
+    set({ "n", "v" }, "<up>", function()
+      mc.addCursor("k")
+    end)
+    set({ "n", "v" }, "<down>", function()
+      mc.addCursor("j")
+    end)
+
+    -- Add a cursor and jump to the next word under cursor.
+    set({ "n", "v" }, "<c-n>", function()
+      mc.addCursor("*")
+    end)
+
+    -- Jump to the next word under cursor but do not add a cursor.
+    set({ "n", "v" }, "<c-s>", function()
+      mc.skipCursor("*")
+    end)
+
+    -- Rotate the main cursor.
+    set({ "n", "v" }, "<left>", mc.nextCursor)
+    set({ "n", "v" }, "<right>", mc.prevCursor)
+
+    -- Delete the main cursor.
+    set({ "n", "v" }, "<leader>x", mc.deleteCursor)
+
+    -- Add and remove cursors with control + left click.
+    set("n", "<c-leftmouse>", mc.handleMouse)
+
+    set({ "n", "v" }, "<c-q>", function()
+      if mc.cursorsEnabled() then
+        -- Stop other cursors from moving.
+        -- This allows you to reposition the main cursor.
+        mc.disableCursors()
+      else
+        mc.addCursor()
+      end
+    end)
+
+    set("n", "<esc>", function()
+      if not mc.cursorsEnabled() then
+        mc.enableCursors()
+      elseif mc.hasCursors() then
+        mc.clearCursors()
+      else
+        -- Default <esc> handler.
+      end
+    end)
+
+    -- Align cursor columns.
+    set("n", "<leader>a", mc.alignCursors)
+
+    -- Split visual selections by regex.
+    set("v", "s", mc.splitCursors)
+
+    -- Append/insert for each line of visual selections.
+    set("v", "I", mc.insertVisual)
+    set("v", "A", mc.appendVisual)
+
+    -- match new cursors within visual selections by regex.
+    set("v", "M", mc.matchCursors)
+
+    -- Rotate visual selection contents.
+    set("v", "<leader>t", function()
+      mc.transposeCursors(1)
+    end)
+    set("v", "<leader>T", function()
+      mc.transposeCursors(-1)
+    end)
+
+    -- -- Customize how cursors look.
+    -- vim.api.nvim_set_hl(0, "MultiCursorCursor", { link = "Cursor" })
+    -- vim.api.nvim_set_hl(0, "MultiCursorVisual", { link = "Visual" })
+    -- vim.api.nvim_set_hl(0, "MultiCursorDisabledCursor", { link = "Visual" })
+    -- vim.api.nvim_set_hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+    -- Customize how cursors look.
+    local hl = vim.api.nvim_set_hl
+    hl(0, "MultiCursorCursor", { reverse = true })
+    hl(0, "MultiCursorVisual", { link = "Visual" })
+    hl(0, "MultiCursorSign", { link = "SignColumn" })
+    hl(0, "MultiCursorMatchPreview", { link = "Search" })
+    hl(0, "MultiCursorDisabledCursor", { reverse = true })
+    hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+    hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
+  end
+}
