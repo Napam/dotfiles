5cd167932d3cd0ca4365822e1fbd9336f5a5da60
Naphat Amundsen <naphat@live.no>
2025-09-08 07:54:05 +0200

    move persistance to essentials, try kanagawa theme, update lazy

--- unix/stow/vim/dot-config/nvim/lua/plugins/essentials/sonokai.lua
+++ unix/stow/vim/dot-config/nvim/lua/plugins/essentials/sonokai.lua
@@ -2,17 +2,17 @@ return {
   {
     "sainnhe/sonokai",
     config = function()
-      vim.cmd([[
-        au ColorScheme * hi Normal ctermbg=none guibg=none
-        au ColorScheme * hi SignColumn ctermbg=none guibg=none
-        au ColorScheme * hi NormalNC ctermbg=none guibg=none
-        au ColorScheme * hi MsgArea ctermbg=none guibg=none
-        au ColorScheme * hi TelescopeBorder ctermbg=none guibg=none
-        au ColorScheme * hi NvimTreeNormal ctermbg=none guibg=none
-        au ColorScheme * hi NvimTreeEndOfBuffer ctermbg=none guibg=none
-      ]])
+      -- vim.cmd([[
+      --   au ColorScheme * hi Normal ctermbg=none guibg=none
+      --   au ColorScheme * hi SignColumn ctermbg=none guibg=none
+      --   au ColorScheme * hi NormalNC ctermbg=none guibg=none
+      --   au ColorScheme * hi MsgArea ctermbg=none guibg=none
+      --   au ColorScheme * hi TelescopeBorder ctermbg=none guibg=none
+      --   au ColorScheme * hi NvimTreeNormal ctermbg=none guibg=none
+      --   au ColorScheme * hi NvimTreeEndOfBuffer ctermbg=none guibg=none
+      -- ]])
 
-      vim.cmd([[colorscheme sonokai]])
+      -- vim.cmd([[colorscheme sonokai]])
     end,
   },
 }
