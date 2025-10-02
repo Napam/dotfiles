4b07f6187c1fceb92348b2c50875e4a42107aa99
Naphat Amundsen <naphat@live.no>
2025-10-02 09:00:24 +0200

    color16

--- unix/shellutils.sh
+++ unix/shellutils.sh
@@ -320,3 +320,119 @@ color256() {
       echo "Usage: $0 [bash|zsh]"
   fi
 }
+
+color16() {
+echo "  On White(47)     On Black(40)     On Default     Color Code"
+
+echo -e "\
+\033[47m\033[1;37m  White        \033[0m  \
+\033[40m\033[1;37m  White        \033[0m  \
+\033[1;37m  White        \033[0m\
+  1;37\
+"
+
+echo -e "\
+\033[47m\033[37m  Light Gray   \033[0m  \
+\033[40m\033[37m  Light Gray   \033[0m  \
+\033[37m  Light Gray   \033[0m  \
+37\
+"
+
+echo -e "\
+\033[47m\033[1;30m  Gray         \033[0m  \
+\033[40m\033[1;30m  Gray         \033[0m  \
+\033[1;30m  Gray         \033[0m  \
+1;30\
+"
+
+echo -e "\
+\033[47m\033[30m  Black        \033[0m  \
+\033[40m\033[30m  Black        \033[0m  \
+\033[30m  Black        \033[0m  \
+30\
+"
+
+echo -e "\
+\033[47m\033[31m  Red          \033[0m  \
+\033[40m\033[31m  Red          \033[0m  \
+\033[31m  Red          \033[0m  \
+31\
+"
+
+echo -e "\
+\033[47m\033[1;31m  Light Red    \033[0m  \
+\033[40m\033[1;31m  Light Red    \033[0m  \
+\033[1;31m  Light Red    \033[0m  \
+1;31\
+"
+
+echo -e "\
+\033[47m\033[32m  Green        \033[0m  \
+\033[40m\033[32m  Green        \033[0m  \
+\033[32m  Green        \033[0m  \
+32\
+"
+
+echo -e "\
+\033[47m\033[1;32m  Light Green  \033[0m  \
+\033[40m\033[1;32m  Light Green  \033[0m  \
+\033[1;32m  Light Green  \033[0m  \
+1;32\
+"
+
+echo -e "\
+\033[47m\033[33m  Brown        \033[0m  \
+\033[40m\033[33m  Brown        \033[0m  \
+\033[33m  Brown        \033[0m  \
+33\
+"
+
+echo -e "\
+\033[47m\033[1;33m  Yellow       \033[0m  \
+\033[40m\033[1;33m  Yellow       \033[0m  \
+\033[1;33m  Yellow       \033[0m  \
+1;33\
+"
+
+echo -e "\
+\033[47m\033[34m  Blue         \033[0m  \
+\033[40m\033[34m  Blue         \033[0m  \
+\033[34m  Blue         \033[0m  \
+34\
+"
+
+echo -e "\
+\033[47m\033[1;34m  Light Blue   \033[0m  \
+\033[40m\033[1;34m  Light Blue   \033[0m  \
+\033[1;34m  Light Blue   \033[0m  \
+1;34\
+"
+
+echo -e "\
+\033[47m\033[35m  Purple       \033[0m  \
+\033[40m\033[35m  Purple       \033[0m  \
+\033[35m  Purple       \033[0m  \
+35\
+"
+
+echo -e "\
+\033[47m\033[1;35m  Pink         \033[0m  \
+\033[40m\033[1;35m  Pink         \033[0m  \
+\033[1;35m  Pink         \033[0m  \
+1;35\
+"
+
+echo -e "\
+\033[47m\033[36m  Cyan         \033[0m  \
+\033[40m\033[36m  Cyan         \033[0m  \
+\033[36m  Cyan         \033[0m  \
+36\
+"
+
+echo -e "\
+\033[47m\033[1;36m  Light Cyan   \033[0m  \
+\033[40m\033[1;36m  Light Cyan   \033[0m  \
+\033[1;36m  Light Cyan   \033[0m  \
+1;36\
+"
+}
