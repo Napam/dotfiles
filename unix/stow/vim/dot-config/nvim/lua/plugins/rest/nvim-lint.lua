77b3364a6055887373be95ce2593a94b4080447c
Naphat Amundsen <naphat@live.no>
2025-10-16 08:57:35 +0200

    nvim lint alloy

--- unix/stow/vim/dot-config/nvim/lua/plugins/rest/nvim-lint.lua
+++ unix/stow/vim/dot-config/nvim/lua/plugins/rest/nvim-lint.lua
@@ -4,7 +4,22 @@ return {
     local nvim_lint = require("lint")
     local group = vim.api.nvim_create_augroup("nvim-lint", { clear = true })
 
-    nvim_lint.linters_by_ft = {}
+    local pattern = '([^:]+): ([^:]+):(%d+):(%d+): (.+)'
+    local groups = { 'severity', 'file', 'lnum', 'col', 'message'}
+
+    nvim_lint.linters.alloy = {
+      name = "alloy-validate",
+      cmd = "alloy",
+      args = { "validate" },
+      stream = 'stderr',
+      ignore_exitcode = true,
+      parser = require("lint.parser").from_pattern(pattern, groups, nil, {
+        source = "alloy-validate",
+        severity = vim.diagnostic.severity.ERROR,
+      })
+    }
+
+    nvim_lint.linters_by_ft = { alloy = { "alloy" } }
 
     vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
       desc = "nvim-lint",
