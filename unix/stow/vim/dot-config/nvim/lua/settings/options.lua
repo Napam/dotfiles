b94942315d3f0371e4660a05c54a6469efaac556
Naphat Amundsen <naphat@live.no>
2025-11-19 09:05:23 +0100

    nvim-ufo, update lazy

--- unix/stow/vim/dot-config/nvim/lua/settings/options.lua
+++ unix/stow/vim/dot-config/nvim/lua/settings/options.lua
@@ -1,9 +1,9 @@
 vim.opt.clipboard = "unnamedplus" -- access system clipboard
-vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
-vim.opt.cursorline = false -- highlight current line
-vim.opt.expandtab = true -- convert tabs to spaces
-vim.opt.ignorecase = true -- ignore case in search patterns
-vim.opt.list = true -- show listchars, such as whitespace
+vim.opt.conceallevel = 0          -- so that `` is visible in markdown files
+vim.opt.cursorline = false        -- highlight current line
+vim.opt.expandtab = true          -- convert tabs to spaces
+vim.opt.ignorecase = true         -- ignore case in search patterns
+vim.opt.list = true               -- show listchars, such as whitespace
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
@@ -17,3 +17,9 @@ vim.opt.wrap = false
 vim.opt.scrolloff = 5
 vim.opt.undofile = true
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+-- This is important to set to use with nvim-ufo
+vim.o.foldcolumn = '0'
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
