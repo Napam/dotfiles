f9b203bc6db01114cbc2b99a497c7eed16c88377
Naphat Amundsen <naphat@live.no>
2025-12-19 09:28:52 +0100

    yaml bash injections, update lazy

---
+++ unix/stow/vim/dot-config/nvim/queries/yaml/injections.scm
@@ -0,0 +1,23 @@
+; Inject shell syntax for command and args blocks in Kubernetes manifests
+(block_mapping_pair
+  key: (flow_node
+    (plain_scalar
+      (string_scalar) @_key))
+  value: (block_node
+    (block_sequence
+      (block_sequence_item
+        (block_node
+          (block_scalar) @injection.content))))
+  (#match? @_key "^(args|command)$")
+  (#set! injection.language "bash"))
+
+; Also inject shell for script-like content in single string values
+(block_mapping_pair
+  key: (flow_node
+    (plain_scalar
+      (string_scalar) @_key))
+  value: (block_node
+    (block_scalar) @injection.content)
+  (#match? @_key "^(script|command|entrypoint)$")
+  (#set! injection.language "bash"))
+
