ac30d2fca433d72ec1dbb6a110605705cd95324b
Naphat Amundsen <naphat@live.no>
2026-05-18 21:21:28 +0200

    improve setuplinks script, git config use id_ed25519.pub instead of id_rsa

--- unix/stow/setuplinks.sh
+++ unix/stow/setuplinks.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env sh
 #shellcheck shell=bash disable=SC2035
 
-mkdir -p "$HOME/.local"
+# Important to premake these dirs such that
+# stow doesn't link things such as .local,
+# or else we get a bunch of stuff in the dotfiles repo.
+
+mkdir -p "$HOME/.local/share"
+mkdir -p "$HOME/.local/state"
+mkdir -p "$HOME/.local/src"
+mkdir -p "$HOME/.local/bin"
 mkdir -p "$HOME/.config"
 
 stow -d . -t "$HOME" --dotfiles -v 2 */
