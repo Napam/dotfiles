34e81885dbe0640112852011236d603c475cafbb
Naphat Amundsen <naphat@live.no>
2026-07-20 07:37:03 +0200

    templ shitftwdith 4 -> 2

--- unix/stow/vim/dot-config/nvim/after/ftplugin/templ.lua
+++ unix/stow/vim/dot-config/nvim/after/ftplugin/templ.lua
@@ -1,5 +1,5 @@
 -- templ fmt uses hard tabs; render them 4 wide. softtabstop=0 so <Tab> inserts a real tab.
 vim.bo.expandtab = false
-vim.bo.tabstop = 4
-vim.bo.shiftwidth = 4
+vim.bo.tabstop = 2
+vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 0
